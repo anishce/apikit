147ea23e265def67d56b0b573f335e4b5cefccfc
Anish <anish.cse7@gmail.com>
2024-06-01 15:47:50 +0530

    Added Email and phone book models

---
+++ src/REST/TaskManagement/TaskManagement.Web.Api/Models/EmailBookModel.cs
@@ -0,0 +1,21 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+// Ignore Spelling: Api
+
+namespace TaskManagement.Web.Api.Models
+{
+    public class EmailBookModel
+    {
+        public EmailBookType Type { get; set; }
+        public string? Email { get; set; }
+    }
+
+    public enum EmailBookType
+    {
+        Official=1,
+        Personal=2
+    }
+}
---
+++ src/REST/TaskManagement/TaskManagement.Web.Api/Models/PhoneBookModel.cs
@@ -0,0 +1,21 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+// Ignore Spelling: Api
+
+namespace TaskManagement.Web.Api.Models
+{
+    public class PhoneBookModel
+    {
+        public PhoneBookType Type { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+
+    public enum PhoneBookType
+    {
+        Office=1,
+        Home=2
+    }
+}
