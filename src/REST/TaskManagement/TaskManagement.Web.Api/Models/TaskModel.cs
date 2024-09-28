63b658e48f1f719ce6f251cb7075756ff763f0c6
Anish CE <anish.cse7@gmail.com>
2024-09-28 14:00:18 +0530

    Create TaskModel.cs

---
+++ src/REST/TaskManagement/TaskManagement.Web.Api/Models/TaskModel.cs
@@ -0,0 +1,21 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+namespace AnishCeDev.TaskManagement.Web.Api.Models
+{
+    public class TaskModel
+    {
+        public long TaskId { get; set; }
+        public string Subject { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? DueDate { get; set; }
+        public DateTime? DateCompleted { get; set; }
+        public List<Category> Categories { get; set; }
+        public Priority Priority { get; set; }
+        public Status Status { get; set; }
+        public List<Link> Links { get; set; }
+        public List<User> Assignees { get; set; }
+    }
+}
