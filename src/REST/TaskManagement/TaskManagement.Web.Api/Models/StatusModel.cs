0285dbb99b4b920642da9ab354694c570f526eeb
Anish CE <anish.cse7@gmail.com>
2024-09-28 14:03:06 +0530

    Create StatusModel.cs

---
+++ src/REST/TaskManagement/TaskManagement.Web.Api/Models/StatusModel.cs
@@ -0,0 +1,15 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+namespace AnishCeDev.TaskManagement.Web.Api.Models
+{
+    public class StatusModel
+    {
+        public long StatusId { get; set; }
+        public string Name { get; set; } = null!;
+        public int Ordinal { get; set; }
+        public List<LinkModel> Links { get; set; } = null!;
+    }
+}
