02d7af840505de82f7dc1920f7978e61e5d18c92
Anish CE <anish.cse7@gmail.com>
2024-09-28 13:49:55 +0530

    Update User.cs

--- src/REST/TaskManagement/TaskManagement.Web.Api/Models/User.cs
+++ src/REST/TaskManagement/TaskManagement.Web.Api/Models/User.cs
@@ -12,18 +12,19 @@ namespace AnishCeDev.TaskManagement.Web.Api.Models
     public class User : BaseModel
     {
         public string FirstName { get; set; } = null!;
-        public string? MiddleName { get; set; };
+        public string? MiddleName { get; set; } = null;
         public string LastName { get; set; } = null!;
         public string UserName = null!;
         public string Password = null!;
         public UserContactModel Contact =null!;
-        repeated UserAddressMessage addresses=8;
-    public bool isActive = 9;
-        public string remarks = 10;
-        public string createdBy = 11;
-        public DateTime CreatedDate = 12;
-        public string modifiedBy = 13;
-        public google.protobuf.Timestamp modifiedDate = 14;
-        public int32 roleId = 16;
+        //repeated UserAddressMessage addresses=8;
+        public bool IsActive { get; set; }
+        public string? Remarks { get; set; } = null;
+        public string CreatedBy { get; set; } = null!;
+        public DateTime? CreatedDate { get; set; } = null!;
+        public string? ModifiedBy { get; set; } = null;
+        public DateTime? ModifiedDate { get; set; } = null;
+
+        public int? RoleId { get; set; } = null!;
     }
 }
