8267c65aaf7504c3e83e10f43ae81c5b81ee7c95
Anish CE <anish.cse7@gmail.com>
2024-09-28 14:00:54 +0530

    Create CategoryModel.cs

---
+++ src/REST/TaskManagement/TaskManagement.Web.Api/Models/CategoryModel.cs
@@ -0,0 +1,15 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+namespace AnishCeDev.TaskManagement.Web.Api.Models
+{
+    public class CategoryModel
+    {
+        public long CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<LinkModel> Links { get; set; }
+    }
+}
