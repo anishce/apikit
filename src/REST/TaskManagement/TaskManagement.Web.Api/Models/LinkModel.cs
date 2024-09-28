41672af6acc69d390c10b747725da8f8b0736f6b
Anish CE <anish.cse7@gmail.com>
2024-09-28 13:59:44 +0530

    Create LinkModel.cs

---
+++ src/REST/TaskManagement/TaskManagement.Web.Api/Models/LinkModel.cs
@@ -0,0 +1,15 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+namespace AnishCeDev.TaskManagement.Web.Api.Models
+{
+    public class LinkModel
+    {
+        public string Rel { get; set; }
+        public string Href { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+    }
+}
