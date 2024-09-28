7e6ca796e62d5863a78300b6e30655d970fe8b88
Anish CE <anish.cse7@gmail.com>
2024-09-28 14:03:52 +0530

    Create UserModel.cs

---
+++ src/REST/TaskManagement/TaskManagement.Web.Api/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+namespace AnishCeDev.TaskManagement.Web.Api.Models
+{
+    public class UserModel
+    {
+        public long UserId { get; set; }
+        public string Username { get; set; } = null!;
+        public string Firstname { get; set; } = null!;
+        public string Lastname { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public List<LinkModel> Links { get; set; } = null!;
+    }
+}
