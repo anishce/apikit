7e484297b64be663253bb3c0b591656679854f59
Anish CE <anish.cse7@gmail.com>
2024-09-29 12:06:08 +0530

    Added REST API client project

---
+++ src/REST/TaskManagement/AnishCeDev.TaskManagement.Web.Api.Client/Program.cs
@@ -0,0 +1,21 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using System;
+
+#pragma warning disable VSSpell001 // Spell Check
+#pragma warning disable VSSpell001 // Spell Check
+namespace AnishCeDev.TaskManagement.Web.Api.Client
+#pragma warning restore VSSpell001 // Spell Check
+#pragma warning restore VSSpell001 // Spell Check
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
