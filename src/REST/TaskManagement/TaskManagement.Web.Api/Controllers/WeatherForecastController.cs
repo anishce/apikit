0da0f6ff3ba72f341fa97e9839176a0a4e3917e5
Anish CE <anish.cse7@gmail.com>
2024-09-29 12:07:16 +0530

    Update WeatherForecastController.cs

--- src/REST/TaskManagement/TaskManagement.Web.Api/Controllers/WeatherForecastController.cs
+++ src/REST/TaskManagement/TaskManagement.Web.Api/Controllers/WeatherForecastController.cs
@@ -5,7 +5,11 @@
 
 using Microsoft.AspNetCore.Mvc;
 
+#pragma warning disable VSSpell001 // Spell Check
+#pragma warning disable VSSpell001 // Spell Check
 namespace AnishCeDev.TaskManagement.Web.Api.Controllers
+#pragma warning restore VSSpell001 // Spell Check
+#pragma warning restore VSSpell001 // Spell Check
 {
     [ApiController]
     [Route("[controller]")]
