c35c805cdd830f52fa9fcbe43f341c55310a47e1
Anish CE <anish.cse7@gmail.com>
2024-09-29 12:15:37 +0530

    Create StatusController.cs

---
+++ src/REST/TaskManagement/TaskManagement.Web.Api/Controllers/StatusController.cs
@@ -0,0 +1,44 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AnishCeDev.TaskManagement.Web.Api.ApplicationServices;
+using Microsoft.AspNetCore.Mvc;
+
+#pragma warning disable VSSpell001 // Spell Check
+#pragma warning disable VSSpell001 // Spell Check
+namespace AnishCeDev.TaskManagement.Web.Api.Controllers
+#pragma warning restore VSSpell001 // Spell Check
+#pragma warning restore VSSpell001 // Spell Check
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StatusController : ControllerBase
+    {
+        private readonly IStatusAppService statusAppService;
+
+#pragma warning disable VSSpell001 // Spell Check
+        public StatusController(IStatusAppService statusAppService)
+#pragma warning restore VSSpell001 // Spell Check
+        {
+            this.statusAppService = statusAppService;    
+        }
+
+        // GET: api/status/1
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var status = await this.statusAppService.GetStatusAsync(id);
+            return Ok(status);
+        }
+
+        // GET: api/status
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var statuses = await this.statusAppService.GetStatusesAsync();
+            return Ok(statuses);
+        }
+    }
+}
