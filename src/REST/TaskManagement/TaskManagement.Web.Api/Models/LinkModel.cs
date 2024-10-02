2d8312e6c6e019e17b562b92fb8eafef3295043d
Anish CE <anish.cse7@gmail.com>
2024-10-02 14:55:16 +0530

    Update LinkModel.cs

--- src/REST/TaskManagement/TaskManagement.Web.Api/Models/LinkModel.cs
+++ src/REST/TaskManagement/TaskManagement.Web.Api/Models/LinkModel.cs
@@ -3,12 +3,18 @@
 // Author: AnishCeDev
 // ************************************************************************
 
+#pragma warning disable VSSpell001 // Spell Check
+#pragma warning disable VSSpell001 // Spell Check
 namespace AnishCeDev.TaskManagement.Web.Api.Models
+#pragma warning restore VSSpell001 // Spell Check
+#pragma warning restore VSSpell001 // Spell Check
 {
     public class LinkModel
     {
         public string Rel { get; set; } = null!;
+#pragma warning disable VSSpell001 // Spell Check
         public string Href { get; set; } = null!;
+#pragma warning restore VSSpell001 // Spell Check
         public string Title { get; set; } = null!;
         public string Type { get; set; } = null!;
     }
