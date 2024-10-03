5f2cdd75c155f15d5b686518c63f84a5f5c17ae4
Anish CE <anish.cse7@gmail.com>
2024-10-03 13:55:05 +0530

    Update Program.cs

--- src/REST/TaskManagement/TaskManagement.Web.Api/Program.cs
+++ src/REST/TaskManagement/TaskManagement.Web.Api/Program.cs
@@ -3,15 +3,23 @@
 // Author: AnishCeDev
 // ************************************************************************
 
+using AnishCeDev.TaskManagement.Web.Api.ApplicationServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//For lower case Web API endpoints
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<IStatusAppService, StatusAppService>();
+builder.Services.AddScoped<IPriorityAppService, PriorityAppService>();
+builder.Services.AddScoped<ICategoryAppService, CategoryAppService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
