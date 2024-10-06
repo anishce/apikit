55995292c8d6968490c78751f33849348f8d43eb
Anish CE <anish.cse7@gmail.com>
2024-10-06 15:57:17 +0530

    Create TaskAppService.cs

---
+++ src/REST/TaskManagement/TaskManagement.Web.Api/ApplicationServices/TaskAppService.cs
@@ -0,0 +1,70 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AnishCeDev.TaskManagement.Web.Api.Models;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace AnishCeDev.TaskManagement.Web.Api.ApplicationServices
+{
+    public class TaskAppService
+    {
+        public async Task<TaskModel> GetTaskAsync(int taskId)
+        {
+
+        }
+
+        private TaskModel GetTaskModel(int taskId)
+        {
+            return new TaskModel
+            {
+                TaskId = taskId,
+                Assignees = new List<UserModel>
+                {
+                    new UserModel
+                    {
+                        UserId = 1,
+                        Firstname="John",
+                        Lastname="Doe",
+                        Email="john.doe@test.test",
+                        Username="john.doe"
+                    },
+                    new UserModel
+                    {
+                        UserId = 2,
+                        Firstname="Kevin",
+                        Lastname="White",
+                        Email="kevin.white@test.test",
+                        Username="kevin.white"
+                    }
+                },
+                Categories = new List<CategoryModel>
+                {
+                    new CategoryModel
+                    {
+                        CategoryId = 1,
+                        Description = "Test",
+                        Links = new List<LinkModel>
+                        {
+                            new LinkModel
+                            {
+                                Href="https://www.google.com",
+                                Rel="",
+                                Title="Google",
+                                Type="HyperLink"
+                            }
+                        },
+                        Name = "HyperLink Category"
+                    }
+                },
+                DateCompleted = DateTime.Now,
+                DueDate = DateTime.Now.AddDays(1),
+                Links = new List<LinkModel>
+                {
+
+                }
+            };
+        }
+    }
+}
