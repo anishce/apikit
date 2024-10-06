9b90b39e507bba36f4efea7275e5486f6e4a20ef
Anish CE <anish.cse7@gmail.com>
2024-10-06 15:43:49 +0530

    Create IUserAppService.cs

---
+++ src/REST/TaskManagement/TaskManagement.Web.Api/ApplicationServices/IUserAppService.cs
@@ -0,0 +1,15 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AnishCeDev.TaskManagement.Web.Api.Models;
+
+namespace AnishCeDev.TaskManagement.Web.Api.ApplicationServices
+{
+    public interface IUserAppService
+    {
+        Task<UserModel> GetUserAsync(int userId);
+        Task<IEnumerable<UserModel>> GetUsersAsync();
+    }
+}
