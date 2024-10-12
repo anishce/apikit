d971da3a53b42c348ff87442d8bd4a8c8e68e7f7
Anish SE <anish.cse7@gmail.com>
2024-10-12 11:42:33 +0530

    Update TasksController.cs

--- src/REST/TaskManagement/TaskManagement.Web.Api/Controllers/TasksController.cs
+++ src/REST/TaskManagement/TaskManagement.Web.Api/Controllers/TasksController.cs
@@ -34,13 +34,13 @@ namespace AnishCeDev.TaskManagement.Web.Api.Controllers
 
         // POST api/<TasksController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post([FromBody] string value)
         {
         }
 
         // PUT api/<TasksController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void  Put(int id, [FromBody] string value)
         {
         }
 
