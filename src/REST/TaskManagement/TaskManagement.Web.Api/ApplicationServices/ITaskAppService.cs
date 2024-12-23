3082c199f926195800b93a710d90eb05e7cd2b7e
Anish CE <anish.cse7@gmail.com>
2024-12-23 13:19:28 +0530

    Create ITaskAppService.cs

---
+++ src/REST/TaskManagement/TaskManagement.Web.Api/ApplicationServices/ITaskAppService.cs
@@ -0,0 +1,16 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AnishCeDev.TaskManagement.Web.Api.Models;
+
+namespace AnishCeDev.TaskManagement.Web.Api.ApplicationServices
+{
+    public interface ITaskAppService
+    {
+        Task AddNewTaskAsync(TaskModel task);
+        Task<TaskModel> GetTaskAsync(int taskId);
+        Task UpdateTaskAsync(TaskModel task);
+    }
+}
