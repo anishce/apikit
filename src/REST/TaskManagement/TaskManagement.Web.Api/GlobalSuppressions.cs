7487d155666057d94b168f8722f046a3ec96aa45
Anish CE <anish.cse7@gmail.com>
2025-02-02 14:12:06 +0530

    Update GlobalSuppressions.cs

--- src/REST/TaskManagement/TaskManagement.Web.Api/GlobalSuppressions.cs
+++ src/REST/TaskManagement/TaskManagement.Web.Api/GlobalSuppressions.cs
@@ -1,7 +1,7 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
 
 using System.Diagnostics.CodeAnalysis;
 
