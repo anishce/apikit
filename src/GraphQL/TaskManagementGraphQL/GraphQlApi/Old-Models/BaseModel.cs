025f8a29a86b7df784dc4f1f18fd8444c632ff99
Anish CE <anish.cse7@gmail.com>
2025-03-14 13:33:38 +0530

    Create BaseModel.cs

---
+++ src/GraphQL/TaskManagementGraphQL/GraphQlApi/Old-Models/BaseModel.cs
@@ -0,0 +1,17 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+// Ignore Spelling: Api
+
+#pragma warning disable VSSpell001 // Spell Check
+namespace AnishCeDev.TaskManagement.Web.Api.Models
+#pragma warning restore VSSpell001 // Spell Check
+{
+    public abstract class BaseModel
+    {
+        public int Id { get; set; }
+        public int version { get; set; }
+    }
+}
