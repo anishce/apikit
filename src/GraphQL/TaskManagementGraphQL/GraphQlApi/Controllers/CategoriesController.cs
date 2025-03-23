83db2e03a3011a7d39be2eabf45055ac948108dd
Anish CE <anish.cse7@gmail.com>
2025-03-23 19:01:09 +0530

    Update CategoriesController.cs

--- src/GraphQL/TaskManagementGraphQL/GraphQlApi/Controllers/CategoriesController.cs
+++ src/GraphQL/TaskManagementGraphQL/GraphQlApi/Controllers/CategoriesController.cs
@@ -5,13 +5,13 @@
 
 // Ignore Spelling: App Anish Api
 
-using AnishCeDev.TaskManagement.Web.Api.ApplicationServices;
-using AnishCeDev.TaskManagement.Web.Api.Models;
+using AnishCeDev.TaskManagement.GraphQL.Api.ApplicationServices;
+using AnishCeDev.TaskManagement.GraphQL.Api.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace AnishCeDev.TaskManagement.Web.Api.Controllers
+namespace AnishCeDev.TaskManagement.GraphQL.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
