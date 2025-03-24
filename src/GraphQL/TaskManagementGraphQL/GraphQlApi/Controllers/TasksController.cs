4fdcacf963dc76d2c0428f2fbb57c2789eb682fb
Anish CE <anish.cse7@gmail.com>
2025-03-24 21:51:33 +0530

    Update TasksController.cs

--- src/GraphQL/TaskManagementGraphQL/GraphQlApi/Controllers/TasksController.cs
+++ src/GraphQL/TaskManagementGraphQL/GraphQlApi/Controllers/TasksController.cs
@@ -3,12 +3,12 @@
 // Author: AnishCeDev
 // ************************************************************************
 
-using AnishCeDev.TaskManagement.Web.Api.ApplicationServices;
+using AnishCeDev.TaskManagement.GraphQL.Api.ApplicationServices;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace AnishCeDev.TaskManagement.Web.Api.Controllers
+namespace AnishCeDev.TaskManagement.GraphQL.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
