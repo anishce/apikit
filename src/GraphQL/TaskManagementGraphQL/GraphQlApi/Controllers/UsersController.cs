0f5bc995cc5029103ec8dd47ead077c3f7a22277
Anish CE <anish.cse7@gmail.com>
2025-03-24 21:51:36 +0530

    Update UsersController.cs

--- src/GraphQL/TaskManagementGraphQL/GraphQlApi/Controllers/UsersController.cs
+++ src/GraphQL/TaskManagementGraphQL/GraphQlApi/Controllers/UsersController.cs
@@ -3,13 +3,13 @@
 // Author: AnishCeDev
 // ************************************************************************
 
-using AnishCeDev.TaskManagement.Web.Api.ApplicationServices;
-using AnishCeDev.TaskManagement.Web.Api.Models;
+using AnishCeDev.TaskManagement.GraphQL.Api.ApplicationServices;
+using AnishCeDev.TaskManagement.GraphQL.Api.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace AnishCeDev.TaskManagement.Web.Api.Controllers
+namespace AnishCeDev.TaskManagement.GraphQL.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
