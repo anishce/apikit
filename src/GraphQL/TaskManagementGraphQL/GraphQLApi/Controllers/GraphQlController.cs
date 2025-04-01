9f773a6337ee2c94e54bcbba734d4e5f34649992
Anish CE <anish.cse7@gmail.com>
2025-04-01 21:04:29 +0530

    Create GraphQlController.cs

---
+++ src/GraphQL/TaskManagementGraphQL/GraphQLApi/Controllers/GraphQlController.cs
@@ -0,0 +1,16 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnishCeDev.TaskManagement.GraphQL.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GraphQlController : ControllerBase
+    {
+    }
+}
