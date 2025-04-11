6c7c5a77188abc49dafd18a36c37466aab0b0f42
Anish CE <anish.cse7@gmail.com>
2025-04-11 22:06:20 +0530

    Create ICategoryAppService.cs

---
+++ src/gRPC/TaskManagementGrpc/TaskManagement.Web.Api/ApplicationServices/ICategoryAppService.cs
@@ -0,0 +1,20 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AnishCeDev.TaskManagement.Web.Api.Models;
+
+namespace AnishCeDev.TaskManagement.Web.Api.ApplicationServices
+{
+    public interface ICategoryAppService
+    {
+        Task AddCategoryAsync(CategoryModel category);
+        Task<CategoryModel> GetCategoryAsync(int categoryId);
+        Task<IEnumerable<CategoryModel>> GetCategoriesAsync();
+        Task RemoveCategoriesAsync(IEnumerable<int> categoryIds);
+        Task RemoveCategoryAsync(int categoryId);
+        Task UpdateCategoriesAsync(IEnumerable<CategoryModel> categories);
+        Task UpdateCategoryAsync(CategoryModel category);
+    }
+}
