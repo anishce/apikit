42fd77a1eeb2daa4e86377c9654d7e7c846ce591
Anish CE <anish.cse7@gmail.com>
2025-05-15 20:32:31 +0530

    Create GlobalSuppressions.cs

---
+++ src/gRPC/TaskManagementGrpc/TaskManagement.Web.Api/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Naming", "VSSpell001:Spell Check", Justification = "<Pending>", Scope = "namespace", Target = "~N:AnishCeDev.TaskManagement.Web.Api.Controllers")]
+[assembly: SuppressMessage("Naming", "VSSpell001:Spell Check", Justification = "<Pending>", Scope = "member", Target = "~M:AnishCeDev.TaskManagement.Web.Api.Controllers.TasksController.#ctor(AnishCeDev.TaskManagement.Web.Api.ApplicationServices.TaskAppService)")]
+[assembly: SuppressMessage("Naming", "VSSpell001:Spell Check", Justification = "<Pending>", Scope = "namespace", Target = "~N:AnishCeDev.TaskManagement.Web.Api.ApplicationServices")]
