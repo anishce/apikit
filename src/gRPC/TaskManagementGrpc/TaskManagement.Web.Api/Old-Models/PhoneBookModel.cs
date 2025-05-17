3d6e3fe82c95216c01ba545c00d9c7dfa5c9b707
Anish CE <anish.cse7@gmail.com>
2025-05-17 12:06:18 +0530

    Create PhoneBookModel.cs

---
+++ src/gRPC/TaskManagementGrpc/TaskManagement.Web.Api/Old-Models/PhoneBookModel.cs
@@ -0,0 +1,23 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+// Ignore Spelling: Api
+
+#pragma warning disable VSSpell001 // Spell Check
+namespace AnishCeDev.TaskManagement.Web.Api.Models
+#pragma warning restore VSSpell001 // Spell Check
+{
+    public class PhoneBookModel
+    {
+        public PhoneBookType Type { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+
+    public enum PhoneBookType
+    {
+        Office = 1,
+        Home = 2
+    }
+}
