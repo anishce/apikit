03e3ca88c5a2e8466d0cd3ccec058fc2f32e55a7
Anish CE <anish.cse7@gmail.com>
2025-05-17 12:06:47 +0530

    Create WeatherForecast.cs

---
+++ src/gRPC/TaskManagementGrpc/TaskManagement.Web.Api/WeatherForecast.cs
@@ -0,0 +1,20 @@
+// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+// Ignore Spelling: Anish Api
+
+namespace AnishCeDev.TaskManagement.Web.Api
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
