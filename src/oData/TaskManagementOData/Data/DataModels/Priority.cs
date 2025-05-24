b9447f04a92b7d1be5e6ae2b900b9e5d75151d4f
Anish CE <anish.cse7@gmail.com>
2025-05-24 16:22:50 +0530

    Create Priority.cs

---
+++ src/oData/TaskManagementOData/Data/DataModels/Priority.cs
@@ -0,0 +1,22 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnishCeDev.TaskManagement.Data.DataModels
+{
+    public class Priority
+    {
+        public long? PriorityId { get; set; }
+        public string Name { get; set; } = null!;
+        public int Ordinal { get; set; }
+        public List<Link> Links { get; set; } = new List<Link>();
+    }
+
+}
