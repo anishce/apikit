c3f2e8f0156ebba5dfd02117f79092ec44bc7c44
Anish CE <anish.cse7@gmail.com>
2025-05-25 21:42:33 +0530

    Create GlobalSuppressions.cs

---
+++ src/oData/TaskManagementOData/OData.WebApi/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Naming", "VSSpell001:Spell Check", Justification = "<Pending>", Scope = "namespace", Target = "~N:AnishCeDev.TaskManagement.Odata.Api.Controllers")]
+[assembly: SuppressMessage("Naming", "VSSpell001:Spell Check", Justification = "<Pending>", Scope = "member", Target = "~M:AnishCeDev.TaskManagement.Odata.Api.Controllers.TasksController.#ctor(AnishCeDev.TaskManagement.Odata.Api.ApplicationServices.TaskAppService)")]
+[assembly: SuppressMessage("Naming", "VSSpell001:Spell Check", Justification = "<Pending>", Scope = "namespace", Target = "~N:AnishCeDev.TaskManagement.Odata.Api.ApplicationServices")]
