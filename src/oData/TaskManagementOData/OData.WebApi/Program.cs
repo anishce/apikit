d0840250b8bf86809695d154a2a140bedb4f75e9
Anish CE <anish.cse7@gmail.com>
2025-07-02 21:46:40 +0530

    Create Program.cs

---
+++ src/oData/TaskManagementOData/OData.WebApi/Program.cs
@@ -0,0 +1,38 @@
+// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AnishCeDev.TaskManagement.Odata.Api.ApplicationServices;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+//For lower case Web API endpoints
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IStatusAppService, StatusAppService>();
+builder.Services.AddScoped<IPriorityAppService, PriorityAppService>();
+builder.Services.AddScoped<ICategoryAppService, CategoryAppService>();
+builder.Services.AddScoped<IUserAppService, UserAppService>();
+builder.Services.AddScoped<ITaskAppService, TaskAppService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
