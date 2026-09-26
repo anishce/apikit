f85a3dc7842688194c04164fe3dc76ffc06a3c1b
Anish CE <anish.cse7@gmail.com>
2026-09-26 20:27:12 +0530

    Update TasksController.cs

--- src/REST/TaskManagement/TaskManagement.Web.Api/Controllers/TasksController.cs
+++ src/REST/TaskManagement/TaskManagement.Web.Api/Controllers/TasksController.cs
@@ -4,6 +4,7 @@
 // ************************************************************************
 
 using AnishCeDev.TaskManagement.Web.Api.ApplicationServices;
+using AnishCeDev.TaskManagement.Web.Api.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -41,15 +42,18 @@ namespace AnishCeDev.TaskManagement.Web.Api.Controllers
 
         // POST api/<TasksController>
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody] string value)
+        public async Task<IActionResult> Post([FromBody] TaskModel task)
         {
+            await taskAppService.AddNewTaskAsync(task);
             return Created();
         }
 
         // PUT api/<TasksController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put(int id, [FromBody] TaskModel task)
         {
+            await taskAppService.UpdateTaskAsync(id, task).ConfigureAwait(false);
+            return Ok();
         }
 
         // DELETE api/<TasksController>/5
